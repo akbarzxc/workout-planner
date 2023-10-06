fd1bb27724964e15231f4985ed2c6e54d6b88106
Veeti <veeti.pihlava@aalto.fi>
2023-10-06 11:28:08 +0300

    Added git-ignore and removed node_modules

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,8 @@
-import React, { useEffect, useState } from 'react'
-import testService from './services/testService'
+import React from "react";
+import WelcomePage from "./pages/welcomePage";
 
 function App() {
-  const [users, setUsers] = useState([])
-
-  useEffect(() => {
-    testService.getNames().then(names => {
-      setUsers(names)
-      }
-    )
-  }, [])
-
-
-return (
-  <div>
-    <h1>Workout Planner</h1>
-    <h2>Data from API:</h2>
-    <ul>
-      {users?.map((name) => (
-        <li key={name.id}>{name.name}</li>
-      ))}
-    </ul>
-  </div>
-)
-
+  return <WelcomePage />;
 }
 
-export default App
+export default App;
---
+++ client/src/pages/welcomePage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function WelcomePage() {
+  return (
+    <h1 className="text-3xl font-bold underline text-slate-500">
+      Hello world!
+    </h1>
+  );
+}
