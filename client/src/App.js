cf2a63030516536f7c0c9ce0f5001aa087ddc6be
Veeti <veeti.pihlava@aalto.fi>
2023-10-06 12:43:36 +0300

    Styled welcome page

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import WelcomePage from "./pages/welcomePage";
+import SiteLayout from "./components/siteLayout";
 
 function App() {
-  return <WelcomePage />;
+  return (
+    <SiteLayout>
+      <WelcomePage />
+    </SiteLayout>
+  );
 }
 
 export default App;
--- client/src/pages/welcomePage.js
+++ client/src/pages/welcomePage.js
@@ -1,19 +1,24 @@
 import React from "react";
+import WelcomeLayout from "../components/welcomeLayout";
 
 export default function WelcomePage() {
   return (
-    <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
-      <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
-        <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
-          The ultimate tool for planning workouts!
-        </h1>
-        <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
-          Create balanced workouts set towards your goals!
-        </p>
-        <div className="space-x-4">
-          <p>BUTTON HERE</p>
+    <WelcomeLayout>
+      <section className="container grid items-center justify-center gap-6 pt-6 pb-8 md:pt-10 md:pb-12 lg:pt-16 lg:pb-24">
+        <div className="mx-auto flex flex-col items-center gap-4 lg:w-[52rem]">
+          <h1 className="text-3xl text-center font-bold leading-[1.1] tracking-tighter sm:text-5xl md:text-7xl">
+            The ultimate tool for planning workouts!
+          </h1>
+          <p className="max-w-[42rem] leading-normal text-slate-700 sm:text-xl sm:leading-8">
+            Create balanced workouts set towards your goals!
+          </p>
+          <div className="flex gap-4">
+            <button className="relative inline-flex h-11 items-center rounded-md border border-transparent bg-slate-900 px-8 py-2 font-medium text-white hover:bg-slate-600 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2">
+              Get Started
+            </button>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </WelcomeLayout>
   );
 }
