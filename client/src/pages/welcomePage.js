22f0dda76c517aa0586566ef77cff1fd410a8c24
Veeti <veeti.pihlava@aalto.fi>
2023-10-06 11:33:40 +0300

    Added tailwindcss to project

--- client/src/pages/welcomePage.js
+++ client/src/pages/welcomePage.js
@@ -2,8 +2,6 @@ import React from "react";
 
 export default function WelcomePage() {
   return (
-    <h1 className="text-3xl font-bold underline text-slate-500">
-      Hello world!
-    </h1>
+    <h1 className="text-3xl font-bold underline text-blue-500">Hello world!</h1>
   );
 }
