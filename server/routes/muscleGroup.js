bfcd9363a64b909d920e9141cd053b59ba7872d9
markuslippo <71636573+markuslippo@users.noreply.github.com>
2023-10-09 13:44:18 +0300

    Database connection

---
+++ server/routes/muscleGroup.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+const db = require('../db')
+
+
+router.get('/', (req, res) => {
+  db.any("SELECT * FROM muscle_group;")
+  .then(rows => {
+    res.json(rows)
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
+
+module.exports = router
