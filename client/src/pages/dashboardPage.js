6745aebef84e3de30418a8c70cfe589f8f61077d
Veeti <veeti.pihlava@aalto.fi>
2023-10-17 14:20:24 +0300

    Added links between workoutplanner and dashboard

--- client/src/pages/dashboardPage.js
+++ client/src/pages/dashboardPage.js
@@ -1,5 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function DashboardPage() {
-  return <div>dashboardPage</div>;
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <Link to="../workoutplan">Click to view workout planner</Link>
+    </div>
+  );
 }
--- client/src/pages/workoutPlanner.js
+++ client/src/pages/workoutPlanner.js
@@ -1,5 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function WorkoutPlanner() {
-  return <div>workoutPlanner</div>;
+  return (
+    <div>
+      <h1>Workout Planner</h1>
+      <Link to="../dashboard">Click to view dashboard</Link>
+    </div>
+  );
 }
